70ff3dcd273420ba6680474064488b777f92bfed
Peiyuliu <you@example.com>
2025-09-14 23:19:08 +0800

    rebuild _site with correct baseurl configuration

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -1,7 +1,7 @@
 
 const currentUrl = window.location.href;
-const siteUrl = "http://0.0.0.0:4000"; 
-let updatedUrl = currentUrl.replace("http://0.0.0.0:4000", "");
+const siteUrl = "https://george-gca.github.io"; 
+let updatedUrl = currentUrl.replace("https://george-gca.github.io", "");
 if (currentUrl.length == updatedUrl.length && currentUrl.startsWith("http://127.0.0.1")) {
   const otherSiteUrl = siteUrl.replace("localhost", "127.0.0.1");
   updatedUrl = currentUrl.replace(otherSiteUrl + "", "");
