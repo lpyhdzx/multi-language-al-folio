2a50b4f52c22a07ce09ed0160e1a3f3bdf90592f
Peiyuliu <you@example.com>
2025-09-15 00:00:31 +0800

    update URL configuration to use custom domain peiyuliu.cn

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -1,7 +1,7 @@
 
 const currentUrl = window.location.href;
-const siteUrl = "https://george-gca.github.io"; 
-let updatedUrl = currentUrl.replace("https://george-gca.github.io", "");
+const siteUrl = "https://peiyuliu.cn"; 
+let updatedUrl = currentUrl.replace("https://peiyuliu.cn", "");
 if (currentUrl.length == updatedUrl.length && currentUrl.startsWith("http://127.0.0.1")) {
   const otherSiteUrl = siteUrl.replace("localhost", "127.0.0.1");
   updatedUrl = currentUrl.replace(otherSiteUrl + "", "");
